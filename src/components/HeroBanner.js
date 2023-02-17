f6ce50b9bc47d978a173921ab5bb5e1fcf96ab74
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-17 23:41:13 +0300

    Banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -28,7 +28,7 @@ const HeroBanner = () => {
         mb="0px"
         mt="30px"
       >
-        Sweat , Smile <br /> and Repeat
+        Sweat , Smile <br /> And Repeat
       </Typography>
 
       <Typography fontSize="22px" lineHeight="35px" mb={4}>
