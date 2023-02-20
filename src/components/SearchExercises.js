caa49f3350ca794685888ca070d0486f7c5a3fdb
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-20 10:45:09 +0300

    Search

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+
+import { Box, Stack, Button, TextField, Typography } from "@mui/material";
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
-  )
-}
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
+    </Stack>
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
