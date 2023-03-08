f9a7fbb260773d5f8e49dee419d919df6ead1405
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-08 17:19:40 +0300

    Arrows

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,7 +1,30 @@
 import { Stack, Typography } from "@mui/material";
-import { height } from "@mui/system";
-import React from "react";
+import React, { useContext } from "react";
 import Icon from "../assets/icons/gym.png";
+
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+import { VisibilityContext } from "react-horizontal-scrolling-menu";
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
