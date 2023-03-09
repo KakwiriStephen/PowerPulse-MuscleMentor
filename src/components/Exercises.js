0f0a9f23deb5c07b4294a16cb0a8a5cc95ce61d3
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-09 09:57:05 +0300

    Pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import { Pagination } from "@mui/material/Pagination";
+import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 
 import ExerciseCard from "./ExerciseCard";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const excercisePerPage = 9;
+
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px">
@@ -20,6 +23,19 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         {exercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / excercisePerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   );
