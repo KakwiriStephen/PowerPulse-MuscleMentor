e9f224e8ed2a6c9a58f27575a79791fff66d7dd8
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-09 22:48:35 +0300

    Bug

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -12,8 +12,19 @@ const Detail = ({ exerciseDetail }) => {
       sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
     >
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography variant="h3">{name}</Typography>
+        <Typography variant="h6">
+          Exercises keep you strong. <b>{name}</b> {"  "}
+          is one of the best exercises to target your {target}. It will help you
+          improve your mood and gain energy
+        </Typography>
+      </Stack>
     </Stack>
   );
 };
 
 export default Detail;
+
+////////////////////
