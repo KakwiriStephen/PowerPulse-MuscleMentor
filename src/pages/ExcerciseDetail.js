ebf4cefd9836463fd4a614305dd4f08fbbbb1621
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-09 17:34:45 +0300

    Excercise Deatails

--- src/pages/ExcerciseDetail.js
+++ src/pages/ExcerciseDetail.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const ExcerciseDetail = () => {
   return <div>ExcerciseDetail</div>;
