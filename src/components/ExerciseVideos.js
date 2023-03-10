e196512d024ab211ac2b6057a2dad8065c8f2ad4
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-10 14:04:00 +0300

    env Tf

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
-const ExerciseVideos = ({ name, excerciseVideos }) => {
+const ExerciseVideos = ({ name, exerciseVideos }) => {
+  //   if (!exerciseVideos.length) return "Loading...";
+
+  console.log(exerciseVideos);
   return (
     <Box sx={{ marginTop: { lg: "200px", xs: "20px" } }}>
       <Typography variant="h3" mb={"33px"}>
@@ -11,6 +14,37 @@ const ExerciseVideos = ({ name, excerciseVideos }) => {
         </span>{" "}
         exercise videos
       </Typography>
+      <Stack
+        sx={{ flexDirection: { lg: "row" }, gap: { lg: "110px", xs: "0px" } }}
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+      >
+        {exerciseVideos?.slice(0, 3)?.map((item, index) => (
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              style={{ borderTopLeftRadius: "20px" }}
+              src={item.video.thumbnails[0].url}
+              alt={item.video.title}
+            />
+            <Box>
+              <Typography
+                sx={{ fontSize: { lg: "28px", xs: "18px" } }}
+                fontWeight={600}
+                color="#000"
+              >
+                {item.video.title}
+              </Typography>
+            </Box>
+          </a>
+        ))}
+      </Stack>
     </Box>
   );
 };
