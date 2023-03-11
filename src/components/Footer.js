7d31b46a95dbd62f0ae32b11b1498ed3a57e57ba
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-11 11:32:06 +0300

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,10 @@ const Footer = () => {
   return (
     <Box mt="80px" bgcolor="#fff3f4">
       <Stack gap="40px" alignItems="center" px="4px" pt="24px">
-        <img src={Logo} alt="" />
-        <Typography>kakwiri</Typography>
+        <img src={Logo} alt="Logo" width="200px" height="40px" />
+        <Typography variant="h5" pb="40px" mt="20px">
+          kakwiri
+        </Typography>
       </Stack>
     </Box>
   );
