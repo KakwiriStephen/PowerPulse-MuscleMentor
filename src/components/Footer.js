1aa13006b039f5083712ea9d7797c079d1b2b528
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-11 11:24:31 +0300

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="4px" pt="24px">
+        <img src={Logo} alt="" />
+        <Typography>kakwiri</Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
