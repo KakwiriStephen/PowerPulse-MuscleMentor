0e2db5c64c35a3e894a8347dbf30ccbde8f5a37f
KakwiriStephen <ngottakakwiri@gmail.com>
2023-03-11 10:10:04 +0300

    Similar Exercises

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Box, Typography, Stack } from "@mui/material";
+import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SimilarExercises = () => {
-  return <div>SimilarExercises</div>;
+const SimilarExercises = ({ targetMuscleExcercises, equipmentExercises }) => {
+  return (
+    <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
+      <Typography variant="h3">
+        Exercises That Target Similar Muscle Group
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {targetMuscleExcercises.length && (
+          <HorizontalScrollbar data={targetMuscleExcercises} />
+        )}
+      </Stack>
+    </Box>
+  );
 };
 
 export default SimilarExercises;
